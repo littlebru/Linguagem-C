fdcaf64329bb4c58357ec4bdbba813e8645ea63c
Bruna Gomes <brunaclegomes@hotmail.com>
2020-05-01 14:29:20 -0300

    Initial Commit

--- ESTUDOS/05- Condicionais.c
+++ ESTUDOS/05- Condicionais.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Função principal do programa
+//FunÃ§Ã£o principal do programa
 void main(){
 
-    //Definindo Variáveis
+    //Definindo VariÃ¡veis
     int a = 4,opcao = 3;
     float b = 2.5;
     char c = 'x';
@@ -20,7 +20,7 @@ void main(){
         printf("\n A variavel c = letra x");
     }
 
-    //Número par ou impar
+    //NÃºmero par ou impar
     if(a % 2 == 1){
         printf("\n A variavel a eh impar");
     }else{
@@ -36,7 +36,7 @@ void main(){
         printf("\nA opcao nao eh igual a 1 e nem 2");
     }
 
-    //Pausa o programa após executar (Apenas no Windows)
+    //Pausa o programa apÃ³s executar (Apenas no Windows)
     system("pause");
 
 }
