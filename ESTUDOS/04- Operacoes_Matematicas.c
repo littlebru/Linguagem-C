79f4bf768d1a968e2b3f59a01311d4bed2ff43c4
Bruna Gomes <brunaclegomes@hotmail.com>
2020-05-01 14:28:55 -0300

    Initial Commit

--- ESTUDOS/04- Operacoes_Matematicas.c
+++ ESTUDOS/04- Operacoes_Matematicas.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//FunÁ„o principal do programa
+//Fun√ß√£o principal do programa
 void main(){
 
-    //Definindo Vari·veis
+    //Definindo Vari√°veis
     int a = 6, b = 4;
 
     //Soma
     printf("\n A soma de %d e %d = %d",a , b, a + b);
 
-    //SubtraÁ„o
+    //Subtra√ß√£o
     printf("\n A subtracao de %d e %d = %d",a , b, a - b);
 
-    //Divis„o
+    //Divis√£o
     printf("\n A divisao de %d e %d = %d",a , b, a / b);
 
-    //MultiplicaÁ„o
+    //Multiplica√ß√£o
     printf("\n A multiplicacao de %d e %d = %d",a , b, a * b);
 
-    //Resto da Divis„o
+    //Resto da Divis√£o
     printf("\n O resto da divisao entre %d e %d = %d",a , b, a % b);
 
     //Valor Absoluto
     printf("\n O valor absoluto de -3 = %d", abs(-3));
 
-    //Pausa o programa apÛs executar (Apenas no Windows)
+    //Pausa o programa ap√≥s executar (Apenas no Windows)
     system("pause");
 
 }
