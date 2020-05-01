1db10547807866d14e66fed4e7b60d157aee0eb5
Bruna Gomes <brunaclegomes@hotmail.com>
2020-05-01 15:56:06 -0300

    Initial Commit

---
+++ URI ONLINE JUDGE/14- Simple_Product.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+  int NUM1, NUM2, PROD = 0;
+
+  scanf("%d", &NUM1);
+  scanf("%d", &NUM2);
+
+  PROD = NUM1 * NUM2;
+
+  printf("PROD = %d\n", PROD);
+
+return 0;
+}
