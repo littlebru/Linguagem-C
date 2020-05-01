a0ddd2ce6f30ac5b4c7439fe3c3943cfb4d87552
Bruna Gomes <heathjared2@gmail.com>
2020-05-01 14:01:03 -0300

    First Commit :tada:

---
+++ ESTUDOS/ponteiro_constante.c
@@ -0,0 +1,16 @@
+/* ponteirocons.c */
+/* Mostra ponteiros constantes */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+  int i, j, k;
+
+  printf("Imprimindo o endereco das variaveis na memoria\n");
+  printf("Endereco de i: %p\n", &i);          /* O código especial %p é utilizado para imprimir ponteiros, endereço de uma variável*/
+  printf("Endereco de j: %p\n", &j);
+  printf("Endereco de k: %p\n", &k);
+
+  return 0;
+}
---
+++ ESTUDOS/ponteiros.c
@@ -0,0 +1,31 @@
+/* ponteiros.c */
+/* Mostra o uso de ponteiros variáveis */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void reajusta20(float *preco, float *reajuste);      /*protótipo da função*/
+
+int main(){
+  float val_preco, val_reaj;
+
+  do{
+
+    printf("\nInsira o preco atual: ");
+    scanf("%f", &val_preco);
+    reajusta20(&val_preco, &val_reaj);              /*Enviando endereços das variaveis*/
+    printf("\nO preco novo e = %.2f", val_preco);
+    printf("\nO aumento foi de = %.2f\n", val_reaj);
+
+
+  }while(val_preco != 0);
+
+  return 0;
+}
+
+/* reajusta20() */
+/* Reajusta o preço em 20% */
+void reajusta20(float *preco, float *reajuste){
+*reajuste = *preco * 0.2;
+*preco = *preco * 1.2;
+}
